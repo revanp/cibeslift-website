d39cc04b3c0848337025cd08e14f9469e8d5970b
revanp0 <revan.pratama@modena.com>
2023-08-29 00:06:56 +0700

    Update backend layouting

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,17 @@
 <?php
 
+use App\Http\Controllers\Backend\DashboardController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'admin-cms'], function(){
+    Route::get('', [DashboardController::class, 'index']);
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
