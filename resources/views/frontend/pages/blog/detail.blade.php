b8d2b6eab37083dba14b12bc14473a5a71a72935
ade-dtx <93109360+ade-dtx@users.noreply.github.com>
2023-09-12 15:23:05 +0700

    update

---
+++ resources/views/frontend/pages/blog/detail.blade.php
@@ -0,0 +1,76 @@
+@extends('frontend.layouts.app')
+
+@section('content')
+
+    <div class="section">
+        <div class="container">
+            <div class="row justify-content-center mb-5">
+                <div class="col-12 text-center mb-4">
+                    <div class="article-image mb-5"></div>
+
+                    <h2 class="title-50-bold">Beli Lift Cibes, Jangan Kreativ!</h2>
+                </div>
+                <div class="col-12 col-md-8">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis animi ratione laboriosam sapiente non excepturi obcaecati accusamus vitae! Quam expedita earum veritatis numquam facilis veniam velit quisquam odio at saepe! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis animi ratione laboriosam sapiente non excepturi obcaecati accusamus vitae! Quam expedita earum veritatis numquam facilis veniam velit quisquam odio at saepe!</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis animi ratione laboriosam sapiente non excepturi obcaecati accusamus vitae! Quam expedita earum veritatis numquam facilis veniam velit quisquam odio at saepe! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis animi ratione laboriosam sapiente non excepturi obcaecati accusamus vitae! Quam expedita earum veritatis numquam facilis veniam velit quisquam odio at saepe!</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis animi ratione laboriosam sapiente non excepturi obcaecati accusamus vitae! Quam expedita earum veritatis numquam facilis veniam velit quisquam odio at saepe! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis animi ratione laboriosam sapiente non excepturi obcaecati accusamus vitae! Quam expedita earum veritatis numquam facilis veniam velit quisquam odio at saepe!</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis animi ratione laboriosam sapiente non excepturi obcaecati accusamus vitae! Quam expedita earum veritatis numquam facilis veniam velit quisquam odio at saepe! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis animi ratione laboriosam sapiente non excepturi obcaecati accusamus vitae! Quam expedita earum veritatis numquam facilis veniam velit quisquam odio at saepe!</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis animi ratione laboriosam sapiente non excepturi obcaecati accusamus vitae! Quam expedita earum veritatis numquam facilis veniam velit quisquam odio at saepe!</p>
+                    <img src="{{ asset('frontend/images/Cibes_Bringing-people-together_Outside_Night.jpg') }}" class="w-100 my-3" alt="">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis animi ratione laboriosam sapiente non excepturi obcaecati accusamus vitae! Quam expedita earum veritatis numquam facilis veniam velit quisquam odio at saepe! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis animi ratione laboriosam sapiente non excepturi obcaecati accusamus vitae! Quam expedita earum veritatis numquam facilis veniam velit quisquam odio at saepe!</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis animi ratione laboriosam sapiente non excepturi obcaecati accusamus vitae! Quam expedita earum veritatis numquam facilis veniam velit quisquam odio at saepe! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis animi ratione laboriosam sapiente non excepturi obcaecati accusamus vitae! Quam expedita earum veritatis numquam facilis veniam velit quisquam odio at saepe!</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis animi ratione laboriosam sapiente non excepturi obcaecati accusamus vitae! Quam expedita earum veritatis numquam facilis veniam velit quisquam odio at saepe!</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    
+                    <div class="row">
+                        <div class="col-12 mb-5">
+                            <h4 class="title-30-bold">Baca Artikel Lainnya</h4>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-4">
+                            <div class="card-hero mb-5">
+                                <div class="card-hero-thumbnail small-thumbnail"></div>
+                                <div class="card-hero-content">
+                                    <label class="date">12/09/2023</label>
+                                    <h3 class="title-20-bold">Beli Lift Cibes, Jangan Kreativ!</h3>
+                                    <p class="title-20-regular">Lihat instalasi Lift Rumah Cibes bla bla bla bla bla bla bla bla bla bla</p>
+                                    <a href="#" class="link">Read More <i class="fi fi-rr-arrow-small-right"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="card-hero mb-5">
+                                <div class="card-hero-thumbnail small-thumbnail"></div>
+                                <div class="card-hero-content">
+                                    <label class="date">12/09/2023</label>
+                                    <h3 class="title-20-bold">Beli Lift Cibes, Jangan Kreativ!</h3>
+                                    <p class="title-20-regular">Lihat instalasi Lift Rumah Cibes bla bla bla bla bla bla bla bla bla bla</p>
+                                    <a href="#" class="link">Read More <i class="fi fi-rr-arrow-small-right"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="card-hero mb-5">
+                                <div class="card-hero-thumbnail small-thumbnail"></div>
+                                <div class="card-hero-content">
+                                    <label class="date">12/09/2023</label>
+                                    <h3 class="title-20-bold">Beli Lift Cibes, Jangan Kreativ!</h3>
+                                    <p class="title-20-regular">Lihat instalasi Lift Rumah Cibes bla bla bla bla bla bla bla bla bla bla</p>
+                                    <a href="#" class="link">Read More <i class="fi fi-rr-arrow-small-right"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
+
+@push('script')
+@endpush
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,10 @@ use Illuminate\Support\Facades\Auth;
 
 // Frontend
 Route::get('/', [App\Http\Controllers\Frontend\HomeController::class, 'index'])->name('home');
+Route::get('/about-us', [App\Http\Controllers\Frontend\AboutController::class, 'index'])->name('index');
+Route::get('/blog', [App\Http\Controllers\Frontend\BlogController::class, 'index'])->name('index');
+Route::get('/blog/detail', [App\Http\Controllers\Frontend\BlogController::class, 'detail'])->name('detail');
+Route::get('/faq', [App\Http\Controllers\Frontend\FaqController::class, 'index'])->name('index');
 
 // CMS
 Route::group(['prefix' => 'admin-cms'], function(){
