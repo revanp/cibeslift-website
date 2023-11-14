062921efe4fb2c248ac81d2dc0fe3a3fd38667f4
revanp0 <revan.pratama@modena.com>
2023-11-15 05:59:33 +0700

    Update customization

--- database/migrations/2023_11_14_183425_create_product_customization_option.php
+++ database/migrations/2023_11_14_183425_create_product_customization_option.php
@@ -8,18 +8,18 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::create('product_customization_feature_id', function (Blueprint $table) {
+        Schema::create('product_customization_option_id', function (Blueprint $table) {
             $table->id();
             $table->bigInteger('id_product_customization_id');
+            $table->bigInteger('parent_id')->nullable();
             $table->timestamps();
             $table->softDeletes($column = 'deleted_at', $precision = 0);
         });
 
-        Schema::create('product_customization_feature', function (Blueprint $table) {
+        Schema::create('product_customization_option', function (Blueprint $table) {
             $table->id();
-            $table->integer('id_product_customization_feature_id');
+            $table->integer('id_product_customization_option_id');
             $table->string('name');
-            $table->text('description')->nullable();
             $table->string('language_code', 5);
             $table->timestamps();
             $table->softDeletes($column = 'deleted_at', $precision = 0);
@@ -28,7 +28,7 @@ return new class extends Migration
 
     public function down(): void
     {
-        Schema::dropIfExists('product_customization_feature_id');
-        Schema::dropIfExists('product_customization_feature');
+        Schema::dropIfExists('product_customization_option_id');
+        Schema::dropIfExists('product_customization_option');
     }
 };
